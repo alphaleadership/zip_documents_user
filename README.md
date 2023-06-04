619bb32a171ecd8765f1f7b5eb586ae6b175cf95
cybersiel <comptes@cybersiel.com>
2023-06-04 22:15:31 +0200

    Ajout initial des fichiers de projet

---
+++ README.md
@@ -0,0 +1,27 @@
+zip_documents_users
+==============
+
+Description
+------------
+Le projet "zip_documents_users" est un script Python conçu pour archiver les fichiers de tous les utilisateurs de Windows en les compressant dans un fichier ZIP. Le script recherche les fichiers ayant certaines extensions spécifiées et d'une taille inférieure à 20 Mo, puis les ajoute à une archive ZIP. En outre, un fichier de journalisation est créé pour enregistrer les détails de l'opération.
+
+Utilisation
+-----------
+1. Double-cliquez sur le fichier exécutable pour lancer le script.
+2. Le script parcourt les dossiers de tous les utilisateurs de Windows et crée une archive ZIP contenant les fichiers correspondant aux extensions spécifiées.
+3. Un fichier de journalisation, nommé `nom_du_fichier.log`, est généré pour enregistrer les erreurs rencontrées pendant l'opération.
+4. Une fois l'archivage terminé, le fichier ZIP final contenant les fichiers archivés est affiché dans le même répertoire que le fichier exécutable.
+
+Remarque
+--------
+- Assurez-vous d'exécuter le script avec les privilèges d'administrateur afin d'accéder aux dossiers de tous les utilisateurs.
+- Le fichier de journalisation ne contiendra que les erreurs rencontrées pendant l'opération.
+- Les fichiers de journalisation individuels pour chaque utilisateur seront supprimés après l'archivage.
+
+License
+-------
+Ce projet est sous licence MIT. Vous pouvez consulter le fichier "LICENSE" pour plus de détails.
+
+Auteur
+------
+cybersiel
---
+++ zip_documents_users.py
@@ -0,0 +1,84 @@
+import os
+import zipfile
+import socket
+from datetime import datetime
+
+# Liste des noms de dossiers à rechercher
+folder_names = set(['Documents', 'Pictures', 'Downloads', 'Music', 'Videos', 'Desktop'])
+
+# Liste des extensions de fichiers à rechercher
+file_extensions = tuple(['.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf', '.jpg', '.jpeg',
+                   '.png', '.gif', '.bmp', '.tiff', '.mp3', '.wav', '.mp4', '.avi', '.mkv', '.mov', 
+                   '.txt', '.rtf', '.zip', '.rar', '.7z', '.exe', '.dll', '.bat', '.sys', '.ini', 
+                   '.html', '.htm', '.css', '.js', '.php', '.sql', '.xml'])
+
+# Ignorer les dossiers temporaires et cachés
+ignore_folders = tuple(['AppData', 'temp', 'tmp'])
+
+# Nom de la machine
+hostname = socket.gethostname()
+
+# Date et heure actuelles
+current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+# Nom du fichier zip
+zip_filename = f"{hostname}_{current_time}.zip"
+
+# Nom du script lui-même
+script_name = os.path.basename(__file__)
+
+# Chemin complet du fichier zip
+zip_filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), zip_filename)
+
+# Nom du fichier de journalisation pour l'utilisateur courant
+log_filename = f"{hostname}_{current_time}.log"
+
+# Créer un nouveau fichier zip avec une meilleure compression
+with zipfile.ZipFile(zip_filepath, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as myzip:
+    # Chemin complet du répertoire de l'utilisateur courant
+    user_dir = os.path.expanduser('~')
+
+    # Ouvrir le fichier de journalisation avec l'encodage UTF-8
+    with open(log_filename, 'w', encoding='utf-8') as logfile:
+        # Parcourir le répertoire de l'utilisateur courant
+        for foldername, _, filenames in os.walk(user_dir):
+            # Ignorer les dossiers spécifiés et les dossiers cachés
+            if any(ignore_folder in foldername for ignore_folder in ignore_folders) or os.path.basename(foldername).startswith('.'):
+                continue
+
+            # Ignorer le script, le fichier zip lui-même et le fichier de journalisation
+            filenames = [filename for filename in filenames if filename not in {script_name, zip_filename, log_filename}]
+
+            # Vérifier si le dossier ou l'un de ses parents est celui recherché
+            current_folder = foldername
+            while current_folder != user_dir and current_folder != '/':
+                if os.path.basename(current_folder) in folder_names:
+                    # Ajouter tous les fichiers du dossier à l'archive zip
+                    for filename in filenames:
+                        # Chemin complet du fichier
+                        file_path = os.path.join(foldername, filename)
+                        # Vérifier l'extension du fichier
+                        if filename.lower().endswith(file_extensions):
+                            # Vérifier la taille du fichier
+                            try:
+                                file_size = os.path.getsize(file_path)
+                                if file_size <= 20 * 1024 * 1024:  # 20 Mo en octets
+                                    # Ajouter le fichier à l'archive zip
+                                    arcname = os.path.relpath(file_path, user_dir)  # Conserver la structure de l'arborescence dans l'archive zip
+                                    myzip.write(file_path, arcname=arcname)
+                                else:
+                                    logfile.write(f"Ignoré en raison de la taille : {file_path}\n")
+                            except OSError:
+                                logfile.write(f"Erreur lors de la récupération de la taille du fichier : {file_path}\n")
+                    # On a trouvé un dossier correspondant, pas besoin de continuer à chercher
+                    break
+
+                # Monter d'un niveau dans l'arborescence
+                current_folder = os.path.dirname(current_folder)
+
+    # Ajouter le fichier de journalisation au zip
+    myzip.write(log_filename, arcname=log_filename)
+    # Supprimer le fichier de journalisation individuel
+    os.remove(log_filename)
+
+print(f"L'archivage des fichiers est terminé. Les détails sont dans le fichier zip {zip_filename}.")
